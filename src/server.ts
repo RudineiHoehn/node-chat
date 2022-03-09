8b28ffb27fc9cfcd05d90c38a607bb3ab13887ce
Rudinei Luiz Hoehn <rudineilh@gmail.com>
2022-03-08 22:46:33 -0300

    Test

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { serverHttp } from "./http";
 import "./websocket";
 
-serverHttp.listen(3000, () => console.log("Server is running on PORT 3000"));
+const port = process.env.PORT || 3000;
+
+serverHttp.listen(port, () => console.log(`Server is running on PORT ${port}`));
